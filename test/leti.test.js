2e7021553d045e8c038fdc2657f452b57e4ecef9
Jan Bílek <honza@elisiondesign.cz>
2018-07-22 10:43:02 +0200

    Add new tests

--- test/leti.test.js
+++ test/leti.test.js
@@ -1,6 +1,17 @@
 import Leti from '../src/leti.js';
 
-it("Sould store options argument",()=>{
-    const obj = new Leti("options");
-    expect(obj.options).toBe("options");
+it("Sould define options object",()=>{
+    const leti = new Leti();
+    expect(leti.options).toBeDefined();
+    expect(leti.options.types).toBeDefined();
 });
+it("Should define custom notifications",()=>{
+    const options = {
+        types: {
+            custom: {}
+        }
+    };
+    const leti = new Leti(options);
+    expect(leti.custom).toBeDefined();
+    expect(typeof leti.custom === "function").toBeTruthy();
+})
